a93ab1b4903f95464d51339f7001aa5fb589c68f
taejin <taejin5314@gmail.com>
2020-12-31 23:29:06 -0500

    update

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,4 @@
 const fileSystem = require('fs');
 
-fileSystem.writeFileSync('notes.txt', 'My name is Taejin.')
+// fileSystem.writeFileSync('notes.txt', 'My name is Taejin.')
+fileSystem.appendFileSync('notes.txt', '\nWhat is your name?')
