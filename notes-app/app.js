b77504cb8bc564e2bff9dbace9556bf7a75d5783
taejin <taejin5314@gmail.com>
2021-01-01 17:42:32 -0500

    Update app.js

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,6 +3,8 @@ const getNotes = require('./notes.js');
 
 const command = process.argv[2];
 
+console.log(process.argv);
+
 if (command === 'add') {
     console.log('Adding note!');
 } else if (command === 'remove') {
