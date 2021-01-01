e67c61559ebf16e4abac0a145076129e1999d78f
taejin <taejin5314@gmail.com>
2021-01-01 16:14:40 -0500

    validator example

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,10 @@
 const validator = require('validator');
-
 const getNotes = require('./notes.js');
 
 const msg = getNotes();
 
 console.log(msg);
-
+console.log(validator.isEmail('taejin@gamil.com'))
 
 
 // const add = require('./utils.js');
