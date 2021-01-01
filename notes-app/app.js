5eb1f3baef923fad00835da45748a07db7c49b25
taejin <taejin5314@gmail.com>
2021-01-01 17:40:20 -0500

    Update app.js

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,16 +1,10 @@
 const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
+const command = process.argv[2];
 
-console.log(msg);
-
-const chalkMsg = chalk.blue.inverse.bold('Success!')
-console.log(chalkMsg);
-
-console.log(process.argv)
-
-
-// const add = require('./utils.js');
-// const sum = add(4, -2);
-// console.log(sum);
+if (command === 'add') {
+    console.log('Adding note!');
+} else if (command === 'remove') {
+    console.log('Removing note!');
+}
