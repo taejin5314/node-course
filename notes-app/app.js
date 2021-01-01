74d6e0b1cb082b5ee47f39b7b33d8c360f35b98d
taejin <taejin5314@gmail.com>
2021-01-01 16:05:38 -0500

    npm init

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,9 @@
-const firstName = require('./utils.js');
+const getNotes = require('./notes.js');
+const msg = getNotes();
+console.log(msg);
 
-console.log(firstName)
+
+
+// const add = require('./utils.js');
+// const sum = add(4, -2);
+// console.log(sum);
--- notes-app/utils.js
+++ notes-app/utils.js
@@ -2,4 +2,8 @@ console.log('utils.js');
 
 const name = 'Taejin';
 
-module.exports = name;
+const add = function (a, b) {
+    return a + b;
+}
+
+module.exports = add;
