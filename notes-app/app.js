b93a892c6074568501e2a65587a53e4e7341def7
taejin <taejin5314@gmail.com>
2021-01-01 23:58:15 -0500

    update

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,39 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const command = process.argv[2];
+yargs.version('1.1.0');
 
-console.log(process.argv);
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note!');
+    }
+})
 
-if (command === 'add') {
-    console.log('Adding note!');
-} else if (command === 'remove') {
-    console.log('Removing note!');
-}
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List a note',
+    handler: function () {
+        console.log('Listing the note');
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading the note');
+    }
+})
+
+console.log(yargs.argv)
