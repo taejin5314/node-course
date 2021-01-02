14dde13dfe0658433e0e8721d47e20689e9c9614
taejin <taejin5314@gmail.com>
2021-01-02 14:40:20 -0500

    update

--- notes-app/utils.js
+++
@@ -1,9 +0,0 @@
-console.log('utils.js');
-
-const name = 'Taejin';
-
-const add = function (a, b) {
-    return a + b;
-}
-
-module.exports = add;
--- playground/1-json.js
+++ playground/1-json.js
@@ -1,10 +1,21 @@
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-}
+const fs = require('fs')
 
-const bookJSON = JSON.stringify(book);
-console.log(bookJSON);
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+// }
 
-const parsedData = JSON.parse(bookJSON);
-console.log(parsedData.author);
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync('1-json.json', bookJSON);
+
+// const dataBuffer = fs.readFileSync('1-json.json');
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON)
+// console.log(data.title);
+
+const sampleBuffer = fs.readFileSync('1-json.json');
+const sampleData = JSON.parse(sampleBuffer);
+sampleData.name = 'Taejin';
+sampleData.age = '26'
+const overWriteJSON = JSON.stringify(sampleData);
+fs.writeFileSync('1-json.json', overWriteJSON);
