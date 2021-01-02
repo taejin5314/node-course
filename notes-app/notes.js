92292b791f2799361a1251b5ba8d2a59a975f6eb
taejin <taejin5314@gmail.com>
2021-01-02 15:20:17 -0500

    Update notes.js

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const chalk = require('chalk');
 
 const getNotes = function () {
     return 'Your notes...';
@@ -17,9 +18,9 @@ const addNote = function (title, body) {
         })
 
         saveNotes(notes);
-        console.log('New note added!');
+        console.log(chalk.green.inverse('New note added!'));
     } else {
-        console.log('Note title taken!')
+        console.log(chalk.red.inverse('Note title taken!'))
     }
 }
 
@@ -28,6 +29,12 @@ const removeNote = function (title) {
     const notesToKeep = notes.filter(function (note) {
         return note.title !== title
     })
+
+    if (notesToKeep.length === notes.length) {
+        console.log(chalk.red.inverse('No note found!'));
+    } else {
+        console.log(chalk.green.inverse('Note removed!'));
+    }
     saveNotes(notesToKeep)
 }
 
