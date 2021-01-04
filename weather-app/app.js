a8b33781982ba231663e7007a420690db38c617b
taejin <taejin5314@gmail.com>
2021-01-04 17:31:47 -0500

    Update app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,5 +3,6 @@ const request = require('postman-request');
 const url = 'http://api.weatherstack.com/current?access_key=0f7b1b70736d02393b08ee6ede0207bb&query=toronto';
 
 request({ url: url, json: true }, function (error, response) {
-    console.log(response.body.current)
+    // console.log(response.body.current)
+    console.log('It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.');
 })
