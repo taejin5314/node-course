c5800e043a6b842a4ee8060ac64549ad29608578
taejin <taejin5314@gmail.com>
2021-01-04 18:25:47 -0500

    Update app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,5 @@ const url = 'http://api.weatherstack.com/current?access_key=0f7b1b70736d02393b08
 
 request({ url: url, json: true }, function (error, response) {
     // console.log(response.body.current)
-    console.log('It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.');
+    console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.');
 })
