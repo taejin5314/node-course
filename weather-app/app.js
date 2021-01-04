97820b0a84f83679b95702411aab5df528c7f720
taejin <taejin5314@gmail.com>
2021-01-04 17:24:09 -0500

    Update app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,10 +2,6 @@ const request = require('postman-request');
 
 const url = 'http://api.weatherstack.com/current?access_key=0f7b1b70736d02393b08ee6ede0207bb&query=toronto';
 
-request(url, function (error, response, body) {
-    // console.log('Error: ' + error);
-    // console.log('Response: ' + response);
-    // console.log('Body: ' + body);
-    const data = JSON.parse(body);
-    console.log(data);
+request({ url: url, json: true }, function (error, response) {
+    console.log(response.body.current)
 })
