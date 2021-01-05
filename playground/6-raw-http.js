69a55713648370eb513dc468166b96dcb4af4d6b
taejin <taejin5314@gmail.com>
2021-01-05 18:31:58 -0500

    Update 6-raw-http.js

--- playground/6-raw-http.js
+++ playground/6-raw-http.js
@@ -2,9 +2,17 @@ const http = require('http');
 const url = 'http://api.weatherstack.com/current?access_key=0f7b1b70736d02393b08ee6ede0207bb&query=toronto&units=f';
 
 
-http.request(url, (response) => {
+const request = http.request(url, (response) => {
+    let data = ''
 
     response.on('data', (chunk) => {
+        data = data + chunk.toString();
+    })
 
+    response.on('end', () => {
+        const JSONdata = JSON.parse(data);
+        console.log(JSONdata);
     })
 })
+
+request.end();
