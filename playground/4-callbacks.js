011e6637a91a61d7a87d506eadef39a8361f8269
taejin <taejin5314@gmail.com>
2021-01-05 14:25:11 -0500

    Update 4-callbacks.js

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,3 +1,8 @@
 setTimeout(() => {
     console.log('Two seconds are up')
 }, 2000)
+
+const names = ['Taejin', 'Jen', 'Jess'];
+const shortNames = names.filter((name) => {
+    return name.length <= 4;
+})
