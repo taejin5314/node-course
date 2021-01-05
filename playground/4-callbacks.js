ba5d668fb02e077969d98dd536a0ba10a5c1b28c
taejin <taejin5314@gmail.com>
2021-01-05 14:32:00 -0500

    Update 4-callbacks.js

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -6,3 +6,18 @@ const names = ['Taejin', 'Jen', 'Jess'];
 const shortNames = names.filter((name) => {
     return name.length <= 4;
 })
+
+const geocode = (address, callback) => {
+    setTimeout(() => {
+        const data = {
+            latitude: 0,
+            longitude: 0,
+        }
+
+        callback(data)
+    }, 2000)
+}
+
+geocode('Philadelphia', (data) => {
+    console.log(data)
+});
