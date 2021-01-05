095183d62067377c06dbf8cd5ff7e4457dae70a9
taejin <taejin5314@gmail.com>
2021-01-05 14:59:40 -0500

    update

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,6 @@
 const request = require('postman-request');
 const chalk = require('chalk');
+const geocode = require('./utils/geocode');
 
 // const url = 'http://api.weatherstack.com/current?access_key=0f7b1b70736d02393b08ee6ede0207bb&query=toronto&units=f';
 
@@ -23,23 +24,6 @@ const chalk = require('chalk');
 //         console.log(latitude, longitude);
 //     }
 // })
-
-const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoidGFlamluNTMxNCIsImEiOiJja2pqNzRidTQzNnh3MnpuMGUzMm9veGhsIn0.9NGVRrMyQipyH-wwenIsWA';
-
-    request({ url: url, json: true }, (error, response) => {
-        if (error) {
-            callback('Unable to connect to location services!', undefined);
-        } else if (response.body.features.length === 0) {
-            callback('Unable to find location. Try another search', undefined);
-        } else {
-            callback(undefined, {
-                latitude: response.body.features[0].center[0],
-                longitude: response.body.features[0].center[1],
-            })
-        }
-    })
-}
 
 geocode('toronto', (error, data) => {
     console.log('Error', error);
