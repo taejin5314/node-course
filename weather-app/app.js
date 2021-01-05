384926214e7a0cb5f8cdfb1583ca2bf470a55473
taejin <taejin5314@gmail.com>
2021-01-05 17:44:20 -0500

    update

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,24 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('toronto', (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data)
-})
+const address = process.argv[2];
 
-forecast('', '', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+if (!address) {
+    console.log('Please provide an address');
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(error);
+        }
+
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error);
+            }
+
+            console.log(data.location);
+            console.log(forecastData);
+        })
+    })
+}
+
