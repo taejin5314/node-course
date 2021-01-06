bdb78a0dfedd912d1a5f0bfb84c6a8cb16a3b184
taejin <taejin5314@gmail.com>
2021-01-06 15:16:21 -0500

    update

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,14 +7,6 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath))
 
-app.get('/help', (req, res) => {
-    res.send('Help page')
-})
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>');
-})
-
 app.get('/weather', (req, res) => {
     res.send({
         "forecast": "Sunny",
