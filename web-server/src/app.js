6240376b800ac75d2b2406ca22a2ea7c5ad06a9f
taejin <taejin5314@gmail.com>
2021-01-05 21:52:57 -0500

    Update app.js

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -10,6 +10,14 @@ app.get('/help', (req, res) => {
     res.send('Help page')
 })
 
+app.get('/about', (req, res) => {
+    res.send('About page');
+})
+
+app.get('/weather', (req, res) => {
+    res.send('Weather page')
+})
+
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
 })
