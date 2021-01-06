1fb75e6d90e2aa7950a4b35296ba9fb18af690f6
taejin <taejin5314@gmail.com>
2021-01-06 14:49:48 -0500

    Update app.js

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res) => {
@@ -11,11 +11,14 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page');
+    res.send('<h1>About</h1>');
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page')
+    res.send({
+        "forecast": "Sunny",
+        "location": "Toronto"
+    })
 })
 
 app.listen(3000, () => {
