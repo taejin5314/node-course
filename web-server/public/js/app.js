10a7bef45db8c8c575d521ccc0a2dedd16947b08
taejin <taejin5314@gmail.com>
2021-01-11 18:20:14 -0500

    Update app.js

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,16 +1,5 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://localhost:3000/weather?address=toronto').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location);
-            console.log(data.forecast)
-        }
-    })
-})
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 
@@ -18,5 +7,15 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const location = search.value;
-    console.log(location)
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.location);
+                console.log(data.forecast)
+            }
+        })
+    })
 })
