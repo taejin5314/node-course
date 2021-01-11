2672704a5297934a7491aa0ad1b57688bf065d53
taejin <taejin5314@gmail.com>
2021-01-11 18:13:05 -0500

    Update app.js

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,5 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data);
-    })
-});
-
 fetch('http://localhost:3000/weather?address=toronto').then((response) => {
     response.json().then((data) => {
         if (data.error) {
