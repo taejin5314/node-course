5d2956ceaef18f726c1211b3b35242a4c6042b6b
taejin <taejin5314@gmail.com>
2021-01-11 18:26:24 -0500

    update

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,6 +2,10 @@ console.log('Client side javascript file is loaded!')
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+messageOne.textContent = 'From Javascript'
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
