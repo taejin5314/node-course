04a7bb5d69f3651dcc231b7ae8639060dde96690
taejin <taejin5314@gmail.com>
2021-01-14 15:05:15 -0500

    Update mongodb.js

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -4,4 +4,4 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
-const database = 'task-manager'
+const databaseName = 'task-manager'
