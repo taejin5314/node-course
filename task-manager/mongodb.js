f55f8c0fe7fe5da59e890e7f7cc9c9cdec5be3d9
taejin <taejin5314@gmail.com>
2021-01-14 15:12:57 -0500

    Update mongodb.js

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -5,3 +5,16 @@ const MongoClient = mongodb.MongoClient;
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!');
+    }
+
+    const db = client.db(databaseName);
+    db.collection('users').insertOne({
+        name: 'Taejin',
+        age: 26
+    })
+})
