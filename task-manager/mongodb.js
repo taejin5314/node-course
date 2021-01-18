7288959638463ba396902ec9bff454daa740386f
taejin <taejin5314@gmail.com>
2021-01-18 14:51:22 -0500

    Update mongodb.js

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,11 +1,16 @@
 // CRUD create read update delete
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
 
-const connectionURL = 'mongodb://127.0.0.1:27017'
-const databaseName = 'task-manager'
+const { MongoClient, ObjectID } = require('mongodb');
 
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+const id = new ObjectID()
+console.log(id);
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
