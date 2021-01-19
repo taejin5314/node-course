1fd31ae4c0068bd88bbc34b697b72d9fd6d0ce5c
taejin <taejin5314@gmail.com>
2021-01-18 23:41:17 -0500

    Update 4-callbacks.js

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -43,3 +43,16 @@
 //     console.log(sum) // Should print: 5
 // })
 
+const doWorkCallback = (callback) => {
+    setTimeout(() => {
+        callback('This is my error!')
+    }, 2000);
+}
+
+doWorkCallback((error, result) => {
+    if (error) {
+        return console.log(error);
+    }
+
+    console.log(result);
+})
