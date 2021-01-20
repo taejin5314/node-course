64a7c5ae12eae745a0d3642bac2e36136fe348dc
taejin <taejin5314@gmail.com>
2021-01-20 15:50:56 -0500

    Update mongoose.js

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -5,4 +5,22 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true,
 })
 
-const User = mongoose.model()
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+})
+
+const me = new User({
+    name: 'Taejin',
+    age: 26
+})
+
+me.save().then(() => {
+    console.log(me);
+}).catch((error) => {
+    console.log('Error!', error)
+})
