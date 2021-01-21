b7c8fe30be9a8d277db75c626d6d283d4d5b8f7b
taejin <taejin5314@gmail.com>
2021-01-21 12:17:06 -0500

    Update mongoose.js

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -9,11 +9,14 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
     name: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
     email: {
         type: String,
         required: true,
+        trim: true,
+        lowercase: true,
         validate(value) {
             if (!validator.isEmail(value)) {
                 throw new Error('Email is invalid')
@@ -22,6 +25,7 @@ const User = mongoose.model('User', {
     },
     age: {
         type: Number,
+        default: 0,
         validate(value) {
             if (value < 0) {
                 throw new Error('Age must be a positive number')
@@ -33,7 +37,6 @@ const User = mongoose.model('User', {
 const me = new User({
     name: 'Mike',
     email: 'mike1346@gmail.com',
-    age: 26
 })
 
 me.save().then(() => {
