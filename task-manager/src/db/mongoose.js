ee4ba170768ae41eef1b36ca72df85e374fa5c5c
taejin <taejin5314@gmail.com>
2021-01-27 17:13:58 -0500

    update

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,49 +1,11 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if (value.toLowerCase.includes('password')) {
-                throw new Error('Password should not contain "password"')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    }
-})
+
 
 // const me = new User({
 //     name: 'Mike',
