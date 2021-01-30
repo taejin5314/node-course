a6b55e1bff9f44b451890b1d63a6edc453a09c1b
taejin <taejin5314@gmail.com>
2021-01-30 17:30:12 -0500

    Update 8-promises.js

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,12 +1,19 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        // resolve([1, 4, 7]);
-        reject('This is my error!');
-    }, 2000)
-})
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
 
-doWorkPromise.then((result) => {
-    console.log('Success!', result);
-}).catch((error) => {
-    console.log(error)
-})
+// add(1, 2).then((sum) => {
+//     console.log(sum);
+
+//     add(sum, 5).then((sum2) => {
+//         console.log(sum2);
+//     }).catch((e) => {
+//         console.log(e);
+//     })
+// }).catch((e) => {
+//     console.log(e);
+// })
