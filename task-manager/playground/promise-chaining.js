9399c9b6a4729275dd375537f370b999855390dc
taejin <taejin5314@gmail.com>
2021-01-30 20:34:50 -0500

    Update promise-chaining.js

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -11,3 +11,15 @@ User.findByIdAndUpdate('6009b516d768701970dcd6dc', { age: 1 }).then((user) => {
 }).catch((e) => {
     console.log(e);
 })
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age });
+    const count = await User.countDocuments({ age });
+    return count;
+}
+
+updateAgeAndCount('6009b516d768701970dcd6dc', 2).then((count) => {
+    console.log(count);
+}).catch((e) => {
+    console.log(e);
+})
