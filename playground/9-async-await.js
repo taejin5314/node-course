8d5abe6a250a547dac14be97828b43753b642f65
taejin <taejin5314@gmail.com>
2021-01-30 20:18:53 -0500

    Update 9-async-await.js

--- playground/9-async-await.js
+++ playground/9-async-await.js
@@ -1,5 +1,19 @@
-const doWork = async () => {
-    return 'Taejin'
+const { response } = require("express")
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
 }
 
-console.log(doWork())
+const doWork = async () => {
+    cont sum = await add(1, 99)
+}
+
+doWork().then((result) => {
+    console.log('Result: ', result);
+}).catch((e) => {
+    console.log('Error: ', e)
+})
