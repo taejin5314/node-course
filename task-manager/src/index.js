521cfd1bdc7585b33097cd94f23c1698e8fbfac2
taejin <taejin5314@gmail.com>
2021-01-31 19:52:36 -0500

    Update index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -46,11 +46,11 @@ app.get('/users/:id', async (req, res) => {
 })
 
 app.patch('/users/:id', async (req, res) => {
-    const updates = Obejct.keys(req.body);
+    const updates = Object.keys(req.body);
     const allowedUpdates = ['name', 'email', 'password', 'age'];
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
-    if (isValidOperation) {
+    if (!isValidOperation) {
         return res.status(400).send({ error: 'Invalid updates!' })
     }
 
