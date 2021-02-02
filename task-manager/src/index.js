07f11be09efc38ce4029b14e7d9aa36c9dc483d1
taejin <taejin5314@gmail.com>
2021-02-01 20:37:58 -0500

    Update index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -94,13 +94,34 @@ app.get('/tasks/:id', async (req, res) => {
         const task = await Task.findById(_id);
 
         if (!task) {
-            res.status(404).send();
+            return res.status(404).send();
         }
-
         res.status(201).send(task);
     } catch (e) {
         res.status(500).send();
     }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        res.status(400).send({ error: 'Invalid updates!' });
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (e) {
+        res.status(500).send();
+    }
+
 })
 
 app.listen(port, () => {
