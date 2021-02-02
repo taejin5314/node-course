aad40889a0e566e5a9bb70293ef4864eb863d615
taejin <taejin5314@gmail.com>
2021-02-01 21:11:49 -0500

    separating task router

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -3,13 +3,15 @@ require('./db/mongoose');
 const User = require('./models/user');
 const Task = require('./models/task');
 const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
-app.use(userRouter)
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
     console.log('server is up on port: ' + port);
