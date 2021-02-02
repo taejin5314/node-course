443610be3211f0d0e50da997dede95298af5d2e6
taejin <taejin5314@gmail.com>
2021-02-01 21:07:44 -0500

    Update index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 require('./db/mongoose');
 const User = require('./models/user');
 const Task = require('./models/task');
+const userRouter = require('./routers/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
+app.use(userRouter)
 
 app.post('/users', async (req, res) => {
     const user = new User(req.body);
