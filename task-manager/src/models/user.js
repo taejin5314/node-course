192920db077535e7a3018e09ae29a4330b1159dc
taejin <taejin5314@gmail.com>
2021-02-04 13:47:59 -0500

    Update user.js

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -41,6 +41,21 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        throw new Error('Unable to login');
+    }
+}
+
+// Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
     const user = this
 
