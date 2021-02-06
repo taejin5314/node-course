316f099060f344c61c8687045240bbfe4ba9d20d
taejin <taejin5314@gmail.com>
2021-02-06 17:30:15 -0500

    Update index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -15,17 +15,10 @@ app.listen(port, () => {
     console.log('server is up on port: ' + port);
 })
 
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const myFunction = async () => {
-    const password = 'Red12345!';
-    const hashedPassword = await bcrypt.hash(password, 8);
-
-    console.log(password);
-    console.log(hashedPassword);
-
-    const isMatch = await bcrypt.compare('Red12345!', hashedPassword);
-    console.log(isMatch);
+    jwt.sign()
 }
 
 myFunction();
