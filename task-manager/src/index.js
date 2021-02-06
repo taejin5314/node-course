10e34ad8577c59e7f53ab863ab63e85d6468be29
taejin <taejin5314@gmail.com>
2021-02-06 18:28:39 -0500

    Update index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,6 +6,10 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+    console.log(req.method, req.path)
+})
+
 app.use(express.json())
 app.use(userRouter);
 app.use(taskRouter);
