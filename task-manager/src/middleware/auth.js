4216dbc82d995e2d03a9bf9d7222a47da7302f73
taejin <taejin5314@gmail.com>
2021-02-06 18:49:54 -0500

    Update auth.js

--- task-manager/src/middleware/auth.js
+++ task-manager/src/middleware/auth.js
@@ -1,6 +1,14 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
 const auth = async (req, res, next) => {
-    console.log('auth middleware')
-    next();
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, 'thisismynewcourse');
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDFjNDIxMWNlYjQ1MjBiNzQyYTI4ODEiLCJpYXQiOjE2MTI2NTIwOTJ9.hN4inmmq48BZnfL3z6uq1csFPQa4kBcwv30JzdV_ICI' })
+    } catch (e) {
+        res.status(401).send({ error: 'Please authenticate.' })
+    }
 }
 
 module.exports = auth;
