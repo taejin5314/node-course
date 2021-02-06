cdd42d47791aac8408a99dfa6a853a28e9faa190
taejin <taejin5314@gmail.com>
2021-02-06 17:36:57 -0500

    Update index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -18,7 +18,11 @@ app.listen(port, () => {
 const jwt = require('jsonwebtoken');
 
 const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, '');
+    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse');
+    console.log(token)
+
+    const data = jwt.verify(token, 'thisismynewcourse');
+    console.log(data);
 }
 
 myFunction();
