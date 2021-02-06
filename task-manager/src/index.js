5b6e9316d3efab8eb22dbfc5fdde17c6ba43e419
taejin <taejin5314@gmail.com>
2021-02-06 18:35:36 -0500

    middleware challenge coding

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,11 +6,19 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
 app.use((req, res, next) => {
-    if (req.method === 'GET') {
-        res.send('GET requests are disabled')
+    if (req.method) {
+        res.status(503).send('Server is on maintenance');
     } else {
-        next()
+        next();
     }
 })
 
