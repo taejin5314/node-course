9210e4b9e1bc2fcd6760e5d7f328a66a073f0561
taejin <taejin5314@gmail.com>
2021-02-06 17:45:14 -0500

    update

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -18,7 +18,7 @@ app.listen(port, () => {
 const jwt = require('jsonwebtoken');
 
 const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse');
+    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' });
     console.log(token)
 
     const data = jwt.verify(token, 'thisismynewcourse');
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -15,8 +15,9 @@ router.post('/users', async (req, res) => {
 
 router.post('/users/login', async (req, res) => {
     try {
-        const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
+        res.send({ user, token })
     } catch (e) {
         res.status(400).send()
     }
