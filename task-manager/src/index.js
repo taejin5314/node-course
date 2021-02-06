9c4d7c6371691ba990bdae6fcf75446cdc2b619e
taejin <taejin5314@gmail.com>
2021-02-06 18:40:57 -0500

    adding auth middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,21 +6,6 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
-
-app.use((req, res, next) => {
-    if (req.method) {
-        res.status(503).send('Server is on maintenance');
-    } else {
-        next();
-    }
-})
 
 app.use(express.json())
 app.use(userRouter);
