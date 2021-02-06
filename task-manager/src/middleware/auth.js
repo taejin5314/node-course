609fea165891d2179c7183b37a8f1a1a224d8958
taejin <taejin5314@gmail.com>
2021-02-06 18:52:29 -0500

    Update auth.js

--- task-manager/src/middleware/auth.js
+++ task-manager/src/middleware/auth.js
@@ -5,7 +5,14 @@ const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '');
         const decoded = jwt.verify(token, 'thisismynewcourse');
-        const user = await User.findOne({ _id: decoded._id, 'tokens.token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDFjNDIxMWNlYjQ1MjBiNzQyYTI4ODEiLCJpYXQiOjE2MTI2NTIwOTJ9.hN4inmmq48BZnfL3z6uq1csFPQa4kBcwv30JzdV_ICI' })
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+
+        if (!user) {
+            throw new Error()
+        }
+
+        req.user = user;
+        next();
     } catch (e) {
         res.status(401).send({ error: 'Please authenticate.' })
     }
