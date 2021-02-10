bf3dde2a22d844bcecb25837b9840696c2e05c22
taejin <taejin5314@gmail.com>
2021-02-10 16:40:55 -0500

    Update user.js

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -108,7 +108,7 @@ router.patch('/users/:id', async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove();
-        res.send(user);
+        res.send(req.user);
     } catch (e) {
         res.status(500).send();
     }
