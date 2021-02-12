119d41c1b0c47118d4127d8945a4c1a456532f8a
taejin <taejin5314@gmail.com>
2021-02-12 14:58:12 -0500

    Update task.js

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -55,12 +55,12 @@ router.patch('/tasks/:id', auth, async (req, res) => {
     try {
         const task = await Task.findOne({ _id: req.params.id, owner: req.user._id });
 
-        updates.forEach((update) => task[update] = req.body[update]);
-        await task.save()
-
         if (!task) {
             return res.status(404).send();
         }
+
+        updates.forEach((update) => task[update] = req.body[update]);
+        await task.save()
         res.send(task);
     } catch (e) {
         res.status(400).send(e);
