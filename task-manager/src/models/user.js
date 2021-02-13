03d284eeff7812c9203ef6d0fb35a5302c18fe2e
taejin <taejin5314@gmail.com>
2021-02-13 16:51:39 -0500

    Update user.js

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -104,6 +105,8 @@ userSchema.pre('save', async function (next) {
 
 userSchema.pre('remove', async function (next) {
     const user = this;
+
+    await Task.deleteMany({ owner: user._id })
 
     next();
 })
