4d9f5b9caf2b98a9e787d8a41c0aab596664ed77
taejin <taejin5314@gmail.com>
2021-02-15 14:49:38 -0500

    Update index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,7 +8,10 @@ const port = process.env.PORT || 3000;
 
 const multer = require('multer')
 const upload = multer({
-
+    dest: 'images'
+})
+app.post('/upload', upload.single(), (req, res) => {
+    res.send()
 })
 
 app.use(express.json())
