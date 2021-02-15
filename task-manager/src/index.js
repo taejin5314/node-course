7697f2fe0e522964e36d1bc57f494dc267d46189
taejin <taejin5314@gmail.com>
2021-02-15 14:43:50 -0500

    Update index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,6 +6,8 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const multer = require('multer')
+
 
 app.use(express.json())
 app.use(userRouter);
@@ -14,18 +16,3 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('server is up on port: ' + port);
 })
-
-const Task = require('./models/task');
-const User = require('./models/user');
-
-const main = async () => {
-    // const task = await Task.findById('6024602ad08e9e6cb0557e68');
-    // await task.populate('owner').execPopulate();
-    // console.log(task.owner);
-
-    const user = await User.findById('60245de51d264103e049d26a');
-    await user.populate('tasks').execPopulate();
-    console.log(user.tasks);
-}
-
-main();
