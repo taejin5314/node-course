a4ac811b3de838d078dee993269e36865c8dcf88
taejin <taejin5314@gmail.com>
2021-02-15 22:19:30 -0500

    Update index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,7 +14,7 @@ const upload = multer({
     },
     fileFilter(req, file, cb) {
         if (!file.originalname.match(/\.(doc|docx)$/)) {
-            return cb(new Error('Please upload a Word document '));
+            return cb(new Error('Please upload a Word document'));
         }
 
         cb(undefined, true);
