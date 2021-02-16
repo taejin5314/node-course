6a8116fc177d14a189ae504884c7cff81f675141
taejin <taejin5314@gmail.com>
2021-02-15 22:25:09 -0500

    Update user.js

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -112,7 +112,7 @@ const upload = multer({
         fileSize: 1000000
     },
     fileFilter(req, file, cb) {
-        if (!file.originalname.match()) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
             return cb(new Error('Please upload a jpg, jpeg, png file'));
         }
 
