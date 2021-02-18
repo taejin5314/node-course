a0565da3fecf6592787fe846f80604f94181680d
taejin <taejin5314@gmail.com>
2021-02-18 17:43:35 -0500

    Update account.js

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -3,10 +3,3 @@ const sgMail = require('@sendgrid/mail');
 const sendGridAPIKey = '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'
 
 sgMail.setApiKey(sendGridAPIKey);
-
-sgMail.send({
-    to: 'taejin5314@gmail.com',
-    from: 'taejin5314@gmail.com',
-    subject: 'This is my first creation!',
-    text: 'I hope this one actually get to you.'
-})
