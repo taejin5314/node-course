f72c31464c70d1383294f13261d7ed2198aba3e6
taejin <taejin5314@gmail.com>
2021-02-19 18:46:41 -0500

    update environment variables

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -1,8 +1,6 @@
 const sgMail = require('@sendgrid/mail');
 
-const sendGridAPIKey = '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'
-
-sgMail.setApiKey(sendGridAPIKey);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
