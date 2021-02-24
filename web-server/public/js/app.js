15addfd650db7c8d386535ed6b9b37589ee00150
taejin <taejin5314@gmail.com>
2021-02-24 17:57:51 -0500

    Update app.js

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -17,6 +17,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = '';
 
     fetch(`/weather?address=${location}`).then((response) => {
+
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error;
